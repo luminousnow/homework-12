16e3ff47eed8dfe97e4d64c9aac10b6bc73bce7c
Denys Kravchuk <denis.kravchuk.ua@gmail.com>
2020-11-06 22:00:03 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+import './src/scss/style.scss';
+import countryCardTpl from './src/templates/country.hbs';
+
+// getCountryByName('ukraine');
+
+// function getCountryByName(countryName) {
+//   fetch(`https://restcountries.eu/rest/v2/name/${countryName}`).then(
+//     response => {
+//       // console.log(response.json());
+//       return response;
+//     },
+//   );
+// }
+
+// console.log(getCountryByName('ukraine'));
