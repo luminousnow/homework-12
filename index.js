97bf3416c16a5a6a9045406ef192063933356a80
Denys Kravchuk <denis.kravchuk.ua@gmail.com>
2020-11-08 22:54:09 +0200

    видаляє сміття

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 import './src/scss/style.scss';
 import countryCardTpl from './src/templates/country.hbs';
 
-// getCountryByName('ukraine');
+const refs = {
+  textField: document.querySelector('.text'),
+};
 
-// function getCountryByName(countryName) {
-//   fetch(`https://restcountries.eu/rest/v2/name/${countryName}`).then(
-//     response => {
-//       // console.log(response.json());
-//       return response;
-//     },
-//   );
+// console.log(countryCardTpl);
+
+const params = getCountryByName('ukraine');
+
+function getCountryByName(countryName) {
+  fetch(`https://restcountries.eu/rest/v2/name/${countryName}`)
+    .then(response => {
+      // console.log(response.json());
+      return response.json();
+    })
+    .then(countryData => {
+      console.log(countryData);
+      return (markup = countryCardTpl(countryData));
+    });
+}
+
+// markup(params);
+
+// function markup(params) {
+//   return (markup = countryCardTpl(params));
 // }
 
 // console.log(getCountryByName('ukraine'));
