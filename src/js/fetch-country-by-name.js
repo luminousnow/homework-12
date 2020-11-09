bc9a87a0fc08d62d3a1bd3b161676b89f1dddf9e
Denys Kravchuk <denis.kravchuk.ua@gmail.com>
2020-11-10 00:02:55 +0200

    виносить та експортує getCountryByName у окремий файл

---
+++ src/js/fetch-country-by-name.js
@@ -0,0 +1,5 @@
+// отримує доступ до введених даних, формує і вертає json
+export default function getCountryByName(countryName) {
+  const requestLink = 'https://restcountries.eu/rest/v2/name/';
+  return fetch(requestLink + countryName).then(response => response.json());
+}
