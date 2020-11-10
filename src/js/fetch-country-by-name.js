a4fbc4657b41e16487c88a17edc41067343f932e
Denys Kravchuk <denis.kravchuk.ua@gmail.com>
2020-11-10 22:46:31 +0200

    переіменовує

--- src/js/fetch-country-by-name.js
+++
@@ -1,8 +0,0 @@
-const BASE_URL = 'https://restcountries.eu/rest/v2';
-
-// отримує назву країни, повертає json
-export default function getCountryByName(countryName) {
-  const requestLink = `${BASE_URL}/name/${countryName}`;
-
-  return fetch(requestLink).then(response => response.json());
-}
