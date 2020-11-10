d96f8bca7cf82b96aaacf98dc6428e30731de355
Denys Kravchuk <denis.kravchuk.ua@gmail.com>
2020-11-11 00:18:05 +0200

    змінює логіку перевірки на "if & return"

--- src/js/get-markup.js
+++ src/js/get-markup.js
@@ -1,19 +1,24 @@
 // приймає json --> перевіряє масив по умові --> рендерить розмітку по шаблону з отриманих даних
 import countryCardTpl from '../templates/country-card.hbs';
 import countryListTpl from '../templates/country-list.hbs';
+import errorMessage from '../js/notice';
 import getRefs from '../js/get-refs';
 
 const refs = getRefs();
 
 export default function getMarkup(countryData) {
   if (countryData.length > 10) {
-    console.log('в списку більше 10 країн. плагин pnotify');
+    errorMessage();
     refs.textField.innerHTML = '';
-  } else if (countryData.length >= 2 && countryData.length <= 10) {
+    return;
+  }
+
+  if (countryData.length >= 2 && countryData.length <= 10) {
     const render = countryListTpl(countryData);
     refs.textField.innerHTML = render;
-  } else {
-    const render = countryCardTpl(countryData);
-    refs.textField.innerHTML = render;
+    return;
   }
+
+  const render = countryCardTpl(countryData);
+  refs.textField.innerHTML = render;
 }
