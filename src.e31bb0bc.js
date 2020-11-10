fd18fba3682caaa8bb41990d0c7963ae70005ca4
Denys Kravchuk <denis.kravchuk.ua@gmail.com>
2020-11-10 11:14:51 +0200

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -117,79 +117,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"../node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
-var bundleURL = null;
-
-function getBundleURLCached() {
-  if (!bundleURL) {
-    bundleURL = getBundleURL();
-  }
-
-  return bundleURL;
-}
-
-function getBundleURL() {
-  // Attempt to find the URL of the current script and use that as the base URL
-  try {
-    throw new Error();
-  } catch (err) {
-    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
-
-    if (matches) {
-      return getBaseURL(matches[0]);
-    }
-  }
-
-  return '/';
-}
-
-function getBaseURL(url) {
-  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)\/[^/]+$/, '$1') + '/';
-}
-
-exports.getBundleURL = getBundleURLCached;
-exports.getBaseURL = getBaseURL;
-},{}],"../node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
-var bundle = require('./bundle-url');
-
-function updateLink(link) {
-  var newLink = link.cloneNode();
-
-  newLink.onload = function () {
-    link.remove();
-  };
-
-  newLink.href = link.href.split('?')[0] + '?' + Date.now();
-  link.parentNode.insertBefore(newLink, link.nextSibling);
-}
-
-var cssTimeout = null;
-
-function reloadCSS() {
-  if (cssTimeout) {
-    return;
-  }
-
-  cssTimeout = setTimeout(function () {
-    var links = document.querySelectorAll('link[rel="stylesheet"]');
-
-    for (var i = 0; i < links.length; i++) {
-      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
-        updateLink(links[i]);
-      }
-    }
-
-    cssTimeout = null;
-  }, 50);
-}
-
-module.exports = reloadCSS;
-},{"./bundle-url":"../node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"scss/style.scss":[function(require,module,exports) {
-var reloadCSS = require('_css_loader');
-
-module.hot.dispose(reloadCSS);
-module.hot.accept(reloadCSS);
-},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/handlebars/dist/handlebars.runtime.js":[function(require,module,exports) {
+})({"../node_modules/handlebars/dist/handlebars.runtime.js":[function(require,module,exports) {
 var define;
 var global = arguments[3];
 /**!
@@ -2780,8 +2708,6 @@ function getCountryByName(countryName) {
 },{}],"index.js":[function(require,module,exports) {
 "use strict";
 
-require("./scss/style.scss");
-
 var _country = _interopRequireDefault(require("./templates/country.hbs"));
 
 var _lodash = _interopRequireDefault(require("lodash.debounce"));
@@ -2790,6 +2716,7 @@ var _fetchCountryByName = _interopRequireDefault(require("./js/fetch-country-by-
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+// import './scss/style.scss';
 var refs = {
   textField: document.querySelector('.text'),
   countryInputField: document.querySelector('#countryInputField')
@@ -2810,7 +2737,7 @@ function markup(countryData) {
   var markup = (0, _country.default)(countryData);
   refs.textField.innerHTML = markup;
 }
-},{"./scss/style.scss":"scss/style.scss","./templates/country.hbs":"templates/country.hbs","lodash.debounce":"../node_modules/lodash.debounce/index.js","./js/fetch-country-by-name":"js/fetch-country-by-name.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./templates/country.hbs":"templates/country.hbs","lodash.debounce":"../node_modules/lodash.debounce/index.js","./js/fetch-country-by-name":"js/fetch-country-by-name.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -2838,7 +2765,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64711" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49169" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -3015,4 +2942,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
-//# sourceMappingURL=/homework-12.git/src.e31bb0bc.js.map
+//# sourceMappingURL=/src.e31bb0bc.js.map
