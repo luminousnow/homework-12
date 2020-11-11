7c0cfaeebfdf02d6071ff1da63b9803914fbea6e
Denys Kravchuk <denis.kravchuk.ua@gmail.com>
2020-11-11 16:16:28 +0200

    додає перевірку inputValue перед запитом, дадає коментарі

--- src/index.js
+++ src/index.js
@@ -12,9 +12,14 @@ refs.countryInputField.addEventListener('input', debounce(onInputChange, 500));
 function onInputChange(evt) {
   // відміняє перезавантаження сторінки
   evt.preventDefault();
-
+  // доступ до значення input
   const inputValue = refs.countryInputField.value;
 
+  // перевірка inputValue на наявність даних
+  if (!inputValue) {
+    refs.textField.innerHTML = '';
+    return;
+  }
   // отримує json, рендерить розмітку на сторінку
   getCountry(inputValue).then(getMarkup);
 }
